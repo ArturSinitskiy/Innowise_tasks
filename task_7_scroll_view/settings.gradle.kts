ec7b5fd0e4d1fc4482986f359ccfd8ea10dd29c5
Artur Sinitskiy <sinitskiy.artur@mail.ru>
2024-02-19 19:49:53 +0300

    task 7 scroll view

---
+++ task_7_scroll_view/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "task_7_scroll_view"
+include(":app")
+ 
