f0d438be4608bc329aa659f72a0fab01550f3839
Artur Sinitskiy <sinitskiy.artur@mail.ru>
2024-02-20 18:58:06 +0300

    task 8 relative sizes

---
+++ task_8_relative_sizes/app/src/main/java/com/hfad/task_8_relative_sizes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hfad.task_8_relative_sizes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ task_8_relative_sizes/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "task_8_relative_sizes"
+include(":app")
+ 
